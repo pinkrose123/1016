dcd0a7c6aa293b22027bd68538566772d5229187
pinkrose123 <jiang19970225>
2017-10-16 09:41:02 +0800

    init

---
+++ app/src/main/java/com/example/administrator/demo1016/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.demo1016;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+
+import com.example.administrator.demo1016.R;
+
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+
+public class LoginActivity extends ActivityCompat {
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
